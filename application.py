9530ae77554e37c6953aab000dba59b0cd69ab7e
Fabio Cardoso <fabiocard2002@gmail.com>
2024-05-21 20:58:53 +0100

    Codigo do chat a ver se dá

--- application.py
+++ application.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request, redirect, url_for
 from upload_handler import process_upload
 from get_notas import get_notas
 
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -24,4 +26,7 @@ def add_nota():
 def nova_nota():
     return render_template('nota.html')
 
-app.run()
+if __name__ == "__main__":
+    if os.environ.get('FLASK_ENV') == 'development':
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port)
