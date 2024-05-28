eb36eda3cdae2f6e3e4803a8e281f6410eab83db
CodedByMistake <78051199+CodedByMistake@users.noreply.github.com>
2024-05-28 15:01:28 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Variaveis do CosmoDB
 COSMOS_DB_URL = "https://estbounddb.documents.azure.com:443/"
-COSMOS_DB_KEY = "t2rYwqXYx7UtCOMeSg8QYn8ZHr21hR0p8UwLGqTRILWUJPImUBgi22fShH6YrDvvUorPx1ecDtLNACDbvEIRqA=="
+COSMOS_DB_KEY = "46ErYPThxxLEro6RkZZ2LH5aq9f7LTT5wZ3p9u9BdPTZO1KbVHSWAsdVwJWvLVQ6JkzyX1uZbXBmACDbFLlOeQ=="
 COSMOS_DB_DATABASE_NAME = "notas"
 COSMOS_DB_CONTAINER_NAME = "notasContainer"
 
 # Variaveis do Blob
-BLOB_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=estboundimagens;AccountKey=ohHq7Z0mB1rzGYceFadeFpheaubXhKbwdChbsBdZa3NA54KWuwZmZ5/6RWTst8KT8BX6EM58M3nF+AStlbueKA==;EndpointSuffix=core.windows.net"
+BLOB_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=estboundimagens;AccountKey=wQLak2tzfUJHsOY7n1Lm3DPUPRPZ3RvkvQQrwwEH1jtVbe9PeUoNAhxWsQfkMxFvES+lwf2XAOSI+AStMtL7Pw==;EndpointSuffix=core.windows.net"
 BLOB_CONTAINER_NAME = "fotoscontainer"
